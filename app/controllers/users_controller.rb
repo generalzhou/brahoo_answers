0eced8729a9a606a58eb80d147ac1613e77fd208
dbc-apprentice <apprentice@devbootcamp.com>
2013-06-05 14:20:59 -0500

    Add user controller with new, create and show methods

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,23 @@
+class UsersController < ApplicationController
+  skip_before_filter :require_login
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    user = User.new(params[:user])
+    if user.save
+      session[:current_user_id] = user.id
+      flash[:success] = "You have signed up successfully!"
+      redirect_to user
+    else
+      flash.now[:error] = "Unsuccessful signup attempt!"
+      render :new
+    end
+  end
+  
+  def show
+    @user = User.find(session[:current_user_id])
+  end
+end
