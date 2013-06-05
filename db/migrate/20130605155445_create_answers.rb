b67a615ffd7b25d7e79aad0bd9fa7b5d52bb9efa
Henry Wang <henry.t.wang@gmail.com>
2013-06-05 11:12:46 -0500

    updated answer and question model and migration files

--- db/migrate/20130605155445_create_answers.rb
+++ db/migrate/20130605155445_create_answers.rb
@@ -1,7 +1,8 @@
 class CreateAnswers < ActiveRecord::Migration
   def change
     create_table :answers do |t|
-
+      t.references :question, :null => false
+      t.string     :text, :null => false
       t.timestamps
     end
   end
