83eca91f07841de574f0216b169093d643a51333
dbc-apprentice <apprentice@devbootcamp.com>
2013-06-05 11:16:23 -0500

    created model and migration for User

---
+++ app/models/user.rb
@@ -0,0 +1,18 @@
+class User < ActiveRecord::Base
+
+  attr_accessible :username, :email, :password_digest
+
+  has_secure_password
+
+  has_many :questions
+  has_many :answers
+  has_many :comments
+  has_many :votes
+
+  validates :email, :uniqueness => { :case_sensitive => false }, :format => /\w+@\w+\.\w{2,4}/
+
+  validates :username, :uniqueness  => { :case_sensitive => false }
+
+  validates_confirmation_of :password
+
+end
