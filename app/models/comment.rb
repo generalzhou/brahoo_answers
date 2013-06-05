ae6c5ab90007185993f6c1467be9b8facb58f96c
dbc-apprentice <apprentice@devbootcamp.com>
2013-06-05 11:28:54 -0500

    Fix name error - content changed to text

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,5 +2,5 @@ class Comment < ActiveRecord::Base
   belongs_to :user
   belongs_to :commentable, :polymorphic => true
 
-  attr_accessible :commentable_id, :commentable_type, :content
+  attr_accessible :commentable_id, :commentable_type, :text
 end
