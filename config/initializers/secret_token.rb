b542a060227abf71e72f3c2e122fc983177ecd4e
dbc-apprentice <apprentice@devbootcamp.com>
2013-06-05 10:37:30 -0500

    Skeleton rails app with rspec enabled

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BrahooAnswers::Application.config.secret_token = 'a6e5bbf4bf488c51939b879dcc8385a968db5f8d3cf3af8a68982fd6d50c460019bc2e561df880fb669e2335aeaad3a9f0707037d33a81a5313f98bb1a3a9656'
