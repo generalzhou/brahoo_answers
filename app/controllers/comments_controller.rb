28f35a03ba75bd587ef696bb2eac169db826d1bf
dbc-apprentice <apprentice@devbootcamp.com>
2013-06-05 16:25:08 -0500

    Add create, edit and delete methods to comments controller

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,8 +1,22 @@
 class CommentsController < ApplicationController
 
   def create
+    comment = Comment.new(params[:comment])
+    if comment.save
+      flash[:success] = "Your comment was posted successfully!"
+    else
+      flash.now[:error] = "Unsuccessful comment attempt!"
+    end
+    redirect_to :back
   end
 
   def edit
+    comment = Comment.find(params[:id])
+    redirect_to :back
+  end
+
+  def destroy
+    Comment.find(params[:id]).destroy
+    redirect_to :back
   end
 end
