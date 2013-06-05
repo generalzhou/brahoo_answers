517b19020e482a598bce3827d03e29234bce433b
Henry Wang <henry.t.wang@gmail.com>
2013-06-05 14:42:34 -0500

    updated question and answer model relations

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -1,7 +1,8 @@
 class Answer < ActiveRecord::Base
   attr_accessible :text
 
-  belongs_to :question, :user
+  belongs_to :question
+  belongs_to :user
 
   has_many :comments, :as => :commentable
   has_many :votes, :as => :votable
