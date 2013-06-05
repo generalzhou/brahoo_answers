dfb85d37093e1d5e89ef4866cd588346dcc5cc91
dbc-apprentice <apprentice@devbootcamp.com>
2013-06-05 16:24:49 -0500

    Add create, edit and delete methods to answers controller

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -1,8 +1,22 @@
 class AnswersController < ApplicationController
 
   def create
+    answer = Answer.new(params[:answer])
+    if answer.save
+      flash[:success] = "Your answer was posted successfully!"
+    else
+      flash.now[:error] = "Unsuccessful answer attempt!"
+    end
+    redirect_to :back
   end
 
   def edit
+    answer = Answer.find(params[:id])
+    redirect_to :back
+  end
+
+  def destroy
+    Answer.find(params[:id]).destroy
+    redirect_to :back
   end
 end
