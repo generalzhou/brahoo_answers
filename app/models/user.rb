101a109cdd3fdddee29ea2936b3e961a213e5e80
dbc-apprentice <apprentice@devbootcamp.com>
2013-06-05 11:48:25 -0500

    Add image uploader to app for Carrierwave backed image uploading

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,8 @@
 class User < ActiveRecord::Base
 
   attr_accessible :username, :email, :password_digest, :image
-
+  mount_uploader :image, ImageUploader
+  
   has_secure_password
 
   has_many :questions
