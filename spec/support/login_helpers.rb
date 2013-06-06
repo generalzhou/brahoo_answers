0075642d4fe0170da5ca094bec9f03e316f02b5d
dbc-apprentice <apprentice@devbootcamp.com>
2013-06-06 10:55:35 -0500

    Remove automatic redirect to root_url from login helper

--- spec/support/login_helpers.rb
+++ spec/support/login_helpers.rb
@@ -2,10 +2,10 @@ module LoginHelpers
 
   def user_login(user)
     visit login_path
+    
     fill_in 'session_email',    with: user.email
     fill_in 'session_password', with: "password"
     click_button "Log In"
-    visit root_url
   end
 
 end
