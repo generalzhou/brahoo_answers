78b6f808fef584dec7765221b6deabe04bb0c4da
dbc-apprentice <apprentice@devbootcamp.com>
2013-06-06 14:08:40 -0500

    Fix answer user relationship and add accessible user_id to comments model

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -1,5 +1,5 @@
 class Answer < ActiveRecord::Base
-  attr_accessible :text, :question_id
+  attr_accessible :text, :question_id, :user_id
 
   belongs_to :question
   belongs_to :user
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,5 +2,5 @@ class Comment < ActiveRecord::Base
   belongs_to :user
   belongs_to :commentable, :polymorphic => true
 
-  attr_accessible :commentable_id, :commentable_type, :text
+  attr_accessible :commentable_id, :commentable_type, :text, :user_id
 end
