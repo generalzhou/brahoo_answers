667c7d4643693b0933eeaf65877d36f36d9df0a1
dbc-apprentice <apprentice@devbootcamp.com>
2013-06-05 20:22:05 -0500

    built basic question_controller and all related views

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,2 +1,47 @@
 class QuestionsController < ApplicationController
+  skip_before_filter :require_login, :only => [:index, :show]
+
+  def index
+  end
+
+  def new
+    @question = Question.new
+  end
+
+  def create
+    @question = Question.new(params[:question], :user => current_user)
+    if @question.valid?
+      flash[:success] = "Your question got added brah"
+      @question.save!
+      redirect_to @question
+    else
+      flash.now[:error] = "Bro, your question makes no sense"
+      render :new
+    end
+  end
+
+  def show
+    @question = Question.find_by_id(params[:id])
+  end
+
+  def edit
+    @question = Question.find_by_id(params[:id])
+  end
+
+  def update
+    question = Question.find(params[:id])
+    if question.update_attributes(params[:question], :user => current_user)
+      flash[:success] = "Your question got edited brah"
+      redirect_to question
+    else
+      flash.now[:error] = "Naw bro that ain't gonna work"
+      render :edit
+    end
+  end
+
+  def destroy
+    Question.find_by_id(params[:id]).destroy
+    redirect_to root_path
+  end
+
 end
