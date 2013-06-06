7e954f2a7bcf9fd62e29e0fcb79cf915b3aee950
dbc-apprentice <apprentice@devbootcamp.com>
2013-06-06 10:49:16 -0500

    Fix for login_helper to properly log in user

--- spec/support/login_helpers.rb
+++ spec/support/login_helpers.rb
@@ -2,9 +2,9 @@ module LoginHelpers
 
   def user_login(user)
     visit login_path
-    fill_in 'email',   with: user.email
-    fill_in 'password', with: "password"
-    click_button "Login"
+    fill_in 'session_email',    with: user.email
+    fill_in 'session_password', with: "password"
+    click_button "Log In"
     visit root_url
   end
 
