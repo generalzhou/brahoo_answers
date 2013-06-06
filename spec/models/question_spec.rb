e0682fd46ff3dbc036a19a8292affc576c4e51b4
dbc-apprentice <apprentice@devbootcamp.com>
2013-06-06 12:51:29 -0500

    Fix error in rspec for question; added user to question creation

--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -1,19 +1,21 @@
 require 'spec_helper'
 
 describe 'Question' do
+  before(:each) do
+    @user = FactoryGirl.create(:user)
+  end
 
-    it 'should create a new question' do
+  it 'should create a new question' do
+    expect { FactoryGirl.create(:question, :user => @user) }.to change(Question, :count).by(1)
+  end
 
-      expect { FactoryGirl.create(:question) }.to change(Question, :count).by(1)
-    end
+  it 'should require a title' do
+    FactoryGirl.build(:question, :title => '', :user => @user).should_not be_valid
+  end
 
-    it 'should require a title' do
-      FactoryGirl.build(:question, :title => '').should_not be_valid
-    end
-
-    it 'should require a user author' do
-      FactoryGirl.build(:question, :user => nil).should_not be_valid
-    end
+  it 'should require a user author' do
+    FactoryGirl.build(:question, :user => nil).should_not be_valid
+  end
 
 
 end
