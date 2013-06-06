4204f6661e29506fda588b6896c62aac631b4549
dbc-apprentice <apprentice@devbootcamp.com>
2013-06-05 20:39:21 -0500

    fixed minor errors. Added validation for editing question

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -9,10 +9,11 @@ class QuestionsController < ApplicationController
   end
 
   def create
-    @question = Question.new(params[:question], :user => current_user)
-    if @question.valid?
+    @question = Question.new(params[:question])
+    @question.user = current_user
+
+    if @question.save
       flash[:success] = "Your question got added brah"
-      @question.save!
       redirect_to @question
     else
       flash.now[:error] = "Bro, your question makes no sense"
@@ -21,16 +22,17 @@ class QuestionsController < ApplicationController
   end
 
   def show
-    @question = Question.find_by_id(params[:id])
+    @question = Question.find(params[:id])
   end
 
   def edit
-    @question = Question.find_by_id(params[:id])
+    @question = Question.find(params[:id])
   end
 
   def update
     question = Question.find(params[:id])
-    if question.update_attributes(params[:question], :user => current_user)
+
+    if question.user == current_user && question.update_attributes(params[:question])
       flash[:success] = "Your question got edited brah"
       redirect_to question
     else
@@ -40,7 +42,7 @@ class QuestionsController < ApplicationController
   end
 
   def destroy
-    Question.find_by_id(params[:id]).destroy
+    Question.find(params[:id]).destroy
     redirect_to root_path
   end
 
